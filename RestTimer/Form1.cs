eb86c8ded40adbdf732ef569c230e208410efb62
abagmat <>
2022-06-01 10:24:57 +0300

    cleanup

--- RestTimer/Form1.cs
+++ RestTimer/Form1.cs
@@ -20,16 +20,9 @@ namespace RestTimer
             InitializeComponent();
         }
 
-        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            // Click on the link below to continue learning how to build a desktop app using WinForms!
-            System.Diagnostics.Process.Start("http://aka.ms/dotnet-get-started-desktop");
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Thanks!");
-        }
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    MessageBox.Show("Thanks!");
+        //}
     }
 }
